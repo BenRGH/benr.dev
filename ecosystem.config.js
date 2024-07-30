5c6fad433c5a8ef77352081401d4ccf9a0d69001
BenRGH <benjaminge.franklin@gmail.com>
2024-07-30 18:06:06 +0100

    tidy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,5 @@
 module.exports = {
   apps: [{
-    name: "benrdev",
-    script: "yarn",
-    args: "start",
-    cwd: "/srv/www/test",
-    watch: true,
-    ignore_watch: ["node_modules", ".next"],
-    env: {
-      NODE_ENV: "production",
-    }
-  }]
-};
-
-module.exports = {
-  apps: [{
     name: 'benrdev',
     script: 'npm run start',
     instances: 'max',
