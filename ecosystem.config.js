040934265c73a1948170da7658061bcf5c01e687
Ben <BenRGH@users.noreply.github.com>
2024-11-01 17:38:58 +0000

    Prettier

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-  apps: [{
-    name: 'benrdev',
-    script: 'npm run start',
-    instances: 'max',
-    autorestart: true,
-    watch: true,
-    ignore_watch: ["node_modules", ".next", ".git", ".astro", ".vscode"],
-    max_memory_restart: '1G',
-    cwd: "/srv/www/test",
-    env: {
-      NODE_ENV: 'development',
+  apps: [
+    {
+      name: "benrdev",
+      script: "npm run start",
+      instances: "max",
+      autorestart: true,
+      watch: true,
+      ignore_watch: ["node_modules", ".next", ".git", ".astro", ".vscode"],
+      max_memory_restart: "1G",
+      cwd: "/srv/www/test",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
-    env_production: {
-      NODE_ENV: 'production',
-    }
-  }]
+  ],
 };
