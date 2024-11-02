6e8ed7fb3b987dc79c8bf010587ab6d0d232a8e4
BenRGH <benjaminge.franklin@gmail.com>
2024-11-02 00:30:03 +0000

    Node build settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://benr.dev/',
-  integrations: [preact(), sitemap()],
+  integrations: [sitemap()],
+  output: "hybrid",
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,19 +2,19 @@ module.exports = {
   apps: [
     {
       name: "benrdev",
-      script: "npm run start",
-      instances: "max",
+      script: "./server/entry.mjs",
+      instances: "1",
+      exec_mode : "cluster",
       autorestart: true,
       watch: true,
       ignore_watch: ["node_modules", ".next", ".git", ".astro", ".vscode"],
-      max_memory_restart: "1G",
-      cwd: "/srv/www/test",
+      max_memory_restart: "3G",
+      cwd: "/srv/www/benrdev",
       env: {
         NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
+        HOST: '0.0.0.0',
+        PORT: 3000
+      }
     },
   ],
 };
