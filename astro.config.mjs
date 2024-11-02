77d94aee5c27f81c15a7a4422ad23b2c64847a41
BenRGH <benjaminge.franklin@gmail.com>
2024-11-02 00:45:50 +0000

    Test node deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://benr.dev/',
   integrations: [sitemap()],
+  output: "hybrid",
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
