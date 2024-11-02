366b7b271c6a662c20b28dfe478a92d8954ff505
BenRGH <benjaminge.franklin@gmail.com>
2024-11-02 00:33:42 +0000

    Remove node settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://benr.dev/',
   integrations: [sitemap()],
-  output: "hybrid",
-
-  adapter: node({
-    mode: "standalone"
-  })
 });
